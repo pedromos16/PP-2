369bc7dc12fc994a88fc80254f40a48adfc8d36e
Pedro  Márcio <pedromos16@gmail.com>
2020-06-23 20:42:57 +0000

    Adição das funções festa.

--- main.c
+++ main.c
@@ -117,25 +117,67 @@ void cadastroFuncionario()
     scanf("%d",&func.codigo);
     printf("Digite o nome do funcionário:\n");
     fflush(stdin);
-    gets(func.nome);
+    scanf("%s",&func.nome);
+    printf("Digite o telefone do funcionário:\n");
+    fflush(stdin);
+    scanf("%d",&func.telefone);
     printf("Digite a função do funcionário:\n");
     fflush(stdin);
-    gets(func.funcao);
+    scanf("%s",&func.funcao);
     printf("Digite o tipo do funcionário:\n");
     fflush(stdin);
-    gets(func.tipo);
+    scanf("%s",&func.tipo);
     printf("Digite o salário do funcionário:\n");
     fflush(stdin);
     scanf("%f",&func.salario);
-
+    impRegistro("funcionario");
 }
 void cadastroFornecedor()
 {
-
+    //fornecedor fo;
+    printf("codigo...\n");
+    fflush(stdin);
+    scanf("%d",&forn.codigo);
+    printf("nome...\n");
+    fflush(stdin);
+    //gets(forn.nome);
+    scanf("%s",&forn.nome);
+    printf("telefone...\n");
+    fflush(stdin);
+    scanf("%d",&forn.telefone);
+     printf("Produto fornecido...\n");
+    fflush(stdin);
+    //gets(forn.produtoFornecido);
+    scanf("%s",&forn.nome);
+    //return fo;
+    impRegistro("fornecedor");
 }
 void cadastroFesta()
 {
-
+  printf("Digite o código da festa:\n");
+  fflush(stdin);
+  scanf("%d",&fest.codigoFesta);
+  printf("Digite a quantidade de convidados da festa:\n");
+  fflush(stdin);
+  scanf("%d",&fest.qntConvidados);
+  printf("Digite a data da festa:\n");
+  fflush(stdin);
+  scanf("%d",&fest.data);
+  printf("Digite o dia da semana da festa:\n");
+  fflush(stdin);
+  scanf("%s",&fest.diaSemana);
+  printf("Digite o horário de inicio da festa:\n");
+  fflush(stdin);
+  scanf("%d",&fest.horarioInicio);
+  printf("Digite o horário de fim da festa:\n");
+  fflush(stdin);
+  scanf("%d",&fest.horarioFim);
+  printf("Digite o tema da festa:\n");
+  fflush(stdin);
+  scanf("%s",&fest.tema);
+  printf("Digite o código do cliente:\n");
+  fflush(stdin);
+  scanf("%d",&fest.codigoCliente);
 }
 void impRegistro(char selectRegistro[10])
 {
@@ -149,15 +191,26 @@ void impRegistro(char selectRegistro[10])
     if(selectRegistro=="funcionario"){
         printf("Código do funcionário: %d\n",func.codigo);
         printf("Nome do funcionário: %s\n",func.nome);
+        printf("Telefone do funcionário: %d\n",func.telefone);
         printf("Função do funcionário: %s\n",func.funcao);
         printf("Tipo do funcionário: %s\n",func.tipo);
-        printf("Salário do funcionário: %f\n",func.salario);
+        printf("Salário do funcionário: %.2f\n",func.salario);
     }
     if(selectRegistro=="fornecedor"){
-        
+        printf("Código do fornecedor: %d\n",forn.codigo);
+        printf("Nome do fornecedor: %s\n",forn.nome);
+        printf("Função do fornecedor: %d\n",forn.telefone);
+        printf("Tipo do fornecedor: %s\n",forn.produtoFornecido);
     }
     if(selectRegistro=="festa"){
-
+        printf("Código da festa: %d\n",fest.codigoFesta);
+        printf("Quantidade de convidados da festa: %d\n",fest.qntConvidados);
+        printf("Data da festa: %d\n",fest.data);
+        printf("Dia da semana da festa: %s\n",fest.diaSemana);
+        printf("Horário de inicio da festa: %d\n",fest.horarioInicio);
+        printf("Horário de fim da festa: %d\n",fest.horarioFim);
+        printf("Tema da festa: %s\n",fest.tema);
+        printf("Código do cliente: %d\n",fest.codigoCliente);
     }
 }
 
